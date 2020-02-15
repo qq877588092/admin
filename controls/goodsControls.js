b54539513711285985c59c3a205a58db3bb045ca
niminghui <877588092@qq.com>
2020-02-15 20:37:02 +0800

    API完成

--- controls/goodsControls.js
+++ controls/goodsControls.js
@@ -8,7 +8,76 @@ let insertGoods = async (obj) => {
     return sql;
 }
 
+// 查询全部商品
+let findGoods = async () => {
+    let result = await goodsModel.find()
+    return result
+}
+
+// 删除商品 
+let delGoods = async (_id) => {
+    // _id 就是要删除的商品主键id
+    let result = await goodsModel.deleteOne({
+        _id
+    })
+    return result
+}
+
+// 修改数据
+let updateGoods = async (_id, updateInfo) => {
+    // _id 要修改的主键id  updateInfo 修改的目标数据
+    let result = await goodsModel.updateOne({
+        _id
+    }, updateInfo)
+    return result
+}
+
+// 分页查询
+let findGoodsByPage = async (page, pageSize) => {
+    let allGoods = await goodsModel.find()
+    // 总数据条数
+    let allCount = allGoods.length
+    // 每一页的数据
+    let result = await goodsModel.find().skip((Number(page) - 1) * pageSize).limit(Number(pageSize))
+    return {
+        result,
+        allCount
+    }
+}
+
+//  分类查询
+let findGoodsByType = async (GoodsType) => {
+    let result = await goodsModel.find({
+        GoodsType
+    })
+    return result
+}
+
+// 关键字查询
+let findGoodsByKw = async (kw) => {
+    // 通过正则表达式匹配关键字
+    let regex = new RegExp(kw)
+    let result = await goodsModel.find({
+        $or: [{
+            desc: {
+                $regex: regex
+            }
+        }, {
+            name: {
+                $regex: regex
+            }
+        }]
+    })
+    return result
+}
+
 //抛出函数
 module.exports = {
+    findGoodsByKw,
+    findGoodsByType,
+    findGoodsByPage,
     insertGoods,
+    findGoods,
+    delGoods,
+    updateGoods
 }
--- router/goodsAPI.js
+++ router/goodsAPI.js
@@ -4,7 +4,13 @@ const Router = express.Router();
 
 //引入操作数据库函数
 const {
+    findGoodsByKw,
+    findGoodsByType,
+    findGoodsByPage,
     insertGoods,
+    findGoods,
+    delGoods,
+    updateGoods
 } = require('../controls/goodsControls');
 
 //添加商品
@@ -29,6 +35,143 @@ Router.post('/add', (req, res) => {
             msg: '插入失败请重试'
         })
     });
+})
+
+// 查询全部食品 
+Router.post('/infos', (req, res) => {
+    findGoods()
+        .then((data) => {
+            res.send({
+                list: data,
+                err: 1,
+                msg: '查询成功'
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '查询失败请重试'
+            })
+        })
+})
+
+// 2. 删除商品
+Router.post('/del', (req, res) => {
+    // 获取要删除数据的id
+    let {
+        _id
+    } = req.body
+    delGoods(_id)
+        .then(() => {
+            res.send({
+                err: 1,
+                msg: '删除成功'
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '删除失败请重试'
+            })
+        })
+
+})
+
+// 3. 修改商品
+Router.post('/update', (req, res) => {
+    // 获取修改数据的参数
+    let obj = {
+        _id,
+        title,
+        price,
+        img,
+        shopName,
+        mark
+    } = req.body
+    updateGoods(_id, obj)
+        .then(() => {
+            res.send({
+                err: 1,
+                msg: '修改成功'
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '修改失败请重试'
+            })
+        })
+})
+
+// 分页查询
+Router.post('/getInfos', (req, res) => {
+    let page = req.body.page || 1 //查询的第几页数据
+    let pageSize = req.body.pageSize || 2 //每页几条数据
+    findGoodsByPage(page, pageSize)
+        .then((data) => {
+            console.log(data)
+            let {
+                result,
+                allCount
+            } = data
+            res.send({
+                err: 1,
+                msg: '查询成功',
+                list: result,
+                allCount
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '查询失败请重试'
+            })
+        })
+})
+
+// 分类查询  
+// 分类查询的数据也可能很多 需要和分页查询做关联
+Router.post('/getInfosByType', (req, res) => {
+    let {
+        GoodsType
+    } = req.body
+    findGoodsByType(GoodsType)
+        .then((data) => {
+            res.send({
+                err: 1,
+                msg: '查询成功',
+                list: data
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '查询失败请重试'
+            })
+        })
+
+})
+
+// 模糊查询 关键字查询
+// 也要和分页做关联
+Router.post('/getInfosByKw', (req, res) => {
+    let {
+        kw
+    } = req.body || ''
+    findGoodsByKw(kw)
+        .then((data) => {
+            res.send({
+                err: 1,
+                msg: '查询成功',
+                list: data
+            })
+        })
+        .catch((err) => {
+            res.send({
+                err: 0,
+                msg: '查询失败请重试'
+            })
+        })
 })
 
 //抛出接口
